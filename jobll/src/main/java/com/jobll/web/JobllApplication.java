2e2782feb7764688a13893bf1c95ace7434cf82a
nildr <nildr@DESKTOP-O50JFA4>
2017-02-05 18:50:25 +0900

    css 도입

--- jobll/src/main/java/com/jobll/web/JobllApplication.java
+++ jobll/src/main/java/com/jobll/web/JobllApplication.java
@@ -2,6 +2,7 @@ package com.jobll.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
 public class JobllApplication {
@@ -9,4 +10,6 @@ public class JobllApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JobllApplication.class, args);
 	}
+	
+
 }
