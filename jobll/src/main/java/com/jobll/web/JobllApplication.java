019a80570ab08917c0a6671885befaaeb101e2ab
unknown <>
2017-02-05 12:04:32 +0900

    first commit

---
+++ jobll/src/main/java/com/jobll/web/JobllApplication.java
@@ -0,0 +1,12 @@
+package com.jobll.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobllApplication.class, args);
+	}
+}
